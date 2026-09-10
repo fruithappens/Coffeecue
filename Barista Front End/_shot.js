8cea46d8c4e71fe77a2a8307de96c3c6e4be38ac
Steve Routley <steve.routley@gmail.com>
2026-09-10 17:44:20 +0930

    remove screenshot scratch from the front-end root

--- Barista Front End/_shot.js
+++
@@ -1,9 +0,0 @@
-const { chromium } = require('playwright-core');
-(async () => {
-  const b = await chromium.launch({ channel: 'chrome' });
-  const p = await b.newPage({ viewport: { width: 900, height: 1200 } });
-  await p.goto('file://' + process.argv[2], { waitUntil: 'networkidle' });
-  await p.waitForTimeout(1500);
-  await p.screenshot({ path: process.argv[3], fullPage: true });
-  await b.close();
-})();
